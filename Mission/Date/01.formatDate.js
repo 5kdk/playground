4db52d81a9d84fd78067389e2815e1fd9bc14715
5kdk <tylerk1121@gmail.com>
2023-01-24 23:56:57 +0900

    한국시간 기준으로 변경

--- Mission/Date/01.formatDate.js
+++ Mission/Date/01.formatDate.js
@@ -1,5 +1,7 @@
 /** @type {(date: Date) => string} */
-const formatDate = date => date.toISOString().slice(0, 10);
+
+
+const formatDate = date => new Date(date + new Date().getTimezoneOffset() / 60).toISOString().slice(0, 10);
 
 console.log(formatDate(new Date('2021/07/24'))); // "2021-07-24"
 console.log(formatDate(new Date('1900/1/4'))); // "1900-01-04"
