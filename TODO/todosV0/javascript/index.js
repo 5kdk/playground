d63622592c9bf087cd6adde80a81803a09c941ec
5kdk <tylerk1121@gmail.com>
2023-01-30 13:49:55 +0900

    todos v0

--- TODO/todosV0/javascript/index.js
+++ TODO/todosV0/javascript/index.js
@@ -4,24 +4,30 @@ const $form = getDOM('.todo-form');
 const $todoList = getDOM('.todo-list');
 const $todoInput = getDOM('.todo-input');
 
-const inputChecker = $todoInput => $todoInput.value.trim().replace(/</g, '&lt').replace(/>/g, '&gt');
-
 const addTodo = e => {
   e.preventDefault();
-  const value = inputChecker($todoInput);
+  const value = $todoInput.value.trim().replace(/</g, '&lt').replace(/>/g, '&gt');
   if (!value) return;
   $todoInput.value = '';
+
   $todoInput.focus();
   $todoList.insertAdjacentHTML(
     'afterbegin',
     `
-    <li>
+    <li class="todo-item">
       <input type="checkbox" />
       <span>${value}</span>
-      <button>X</button>
+      
+      <button class="todo-delete-btn">❌</button>
     </li>
     `
   );
 };
 
+const removeTodo = ({ target }) => {
+  if (target.matches('.todo-delete-btn')) return;
+  target.parentNode.remove();
+};
+
 $form.addEventListener('submit', addTodo);
+$todoList.addEventListener('click', removeTodo);
